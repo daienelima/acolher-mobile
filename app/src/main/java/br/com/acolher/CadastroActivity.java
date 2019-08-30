824e24498934a22eadd667f715060f7f9d6a7ec4
Mario Ferreira <mariosilvaosbourne@gmail.com>
2019-08-30 13:09:00 -0300

    Alteracoes - Criando tela de cadastro

---
+++ app/src/main/java/br/com/acolher/CadastroActivity.java
@@ -0,0 +1,56 @@
+package br.com.acolher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.Calendar;
+
+public class CadastroActivity extends AppCompatActivity{
+
+    Calendar calendar;
+    DatePickerDialog datePickerDialog;
+
+    TextInputLayout inputDataNasc;
+    Button btnContinuarCadastro;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+
+        //Configurações da activity
+        setContentView(R.layout.cadastro_basico_activity);
+
+        inputDataNasc = (TextInputLayout) findViewById(R.id.inputDataNasc);
+        btnContinuarCadastro = (Button) findViewById(R.id.buttonContinuarCadastro);
+
+        btnContinuarCadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calendar = Calendar.getInstance();
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+                int month = calendar.get(Calendar.MONTH);
+                int year = calendar.get(Calendar.YEAR);
+
+                datePickerDialog = new DatePickerDialog(CadastroActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int mYear, int mMonth, int mDay) {
+                        inputDataNasc.getEditText().setText(mDay + "/" + (mMonth + 1) + "/" + mYear);
+                    }
+                }, day, month, year);
+                datePickerDialog.show();
+            }
+        });
+    }
+
+}
