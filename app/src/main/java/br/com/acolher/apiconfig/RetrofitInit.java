1d5d01019b21742b76aff5113cfdb114296a0fc5
daienelima <daiene.m17@gmail.com>
2019-09-03 22:56:11 -0300

    Tela de alteração de senha e inclusão de retrofit

---
+++ app/src/main/java/br/com/acolher/apiconfig/RetrofitInit.java
@@ -0,0 +1,24 @@
+package br.com.acolher.apiconfig;
+
+import br.com.acolher.service.ServiceApi;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitInit {
+
+    private String BASE_URL = "http://10.0.2.2:8080/api/";
+    private ServiceApi serviceApi;
+
+    public RetrofitInit() {
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        serviceApi = retrofit.create(ServiceApi.class);
+    }
+
+    public ServiceApi getService(){
+        return serviceApi;
+    }
+}
