6926ecf28282b26c0f6643c59eb738d505cf98f0
Mario Ferreira <mariosilvaosbourne@gmail.com>
2019-09-06 06:51:53 -0300

    Validacao formulario cadastro usuario

--- app/src/main/java/br/com/acolher/view/CadastroActivity.java
+++ app/src/main/java/br/com/acolher/view/CadastroActivity.java
@@ -4,19 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
+
+import br.com.acolher.controller.UsuarioController;
 import br.com.acolher.helper.MaskWatcher;
 
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
 import java.util.Calendar;
 
 import br.com.acolher.R;
+import br.com.acolher.helper.ValidaCPF;
 
 public class CadastroActivity extends AppCompatActivity{
 
@@ -29,8 +34,9 @@ public class CadastroActivity extends AppCompatActivity{
     TextInputLayout inputPassword;
     TextInputLayout inputTelefone;
     TextInputLayout inputCpf;
-    TextInputLayout textInputNome;
-    EditText inputNome;
+    TextInputLayout inputNome;
+    TextInputLayout inputEmail;
+    UsuarioController uc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,21 +45,17 @@ public class CadastroActivity extends AppCompatActivity{
 
         //Configurações da activity
         setContentView(R.layout.cadastro_basico_activity);
-
         inputDataNasc = (TextInputLayout) findViewById(R.id.inputDataNasc);
         btnCalendar = (ImageButton) findViewById(R.id.btnCalendar);
         continuarCadastro = (Button) findViewById(R.id.buttonContinuarCadastro);
         inputPassword = (TextInputLayout) findViewById(R.id.inputPassword);
+        inputCpf = (TextInputLayout) findViewById(R.id.inputCPF);
+        inputCpf.getEditText().addTextChangedListener(new MaskWatcher("(##) #####-####"));
         inputTelefone = (TextInputLayout) findViewById(R.id.inputTelefone);
         inputTelefone.getEditText().addTextChangedListener(new MaskWatcher("(##) #####-####"));
 
-        textInputNome = (TextInputLayout) findViewById(R.id.inputNomeCompleto);
-        inputNome = (EditText) findViewById(R.id.labelNomeCompleto);
-
-
-        inputCpf = (TextInputLayout) findViewById(R.id.inputCPF);
-        inputCpf.getEditText().addTextChangedListener(new MaskWatcher("###.###.###-##"));
-
+        inputNome = (TextInputLayout) findViewById(R.id.inputNomeCompleto);
+        inputEmail = (TextInputLayout) findViewById(R.id.inputEmail);
 
         btnCalendar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -75,6 +77,7 @@ public class CadastroActivity extends AppCompatActivity{
         continuarCadastro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                uc = new UsuarioController();
                 validateForm();
                 //Intent intentEndereco = new Intent(CadastroActivity.this, CadastroEndereco.class);
                 //startActivity(intentEndereco);
@@ -84,6 +87,9 @@ public class CadastroActivity extends AppCompatActivity{
     }
 
     public void openCalendar(){
+
+        inputDataNasc.setErrorEnabled(false);
+
         calendar = Calendar.getInstance();
         int day = calendar.get(Calendar.DAY_OF_MONTH);
         int month = calendar.get(Calendar.MONTH);
@@ -99,14 +105,53 @@ public class CadastroActivity extends AppCompatActivity{
     }
 
     public boolean validateForm(){
-        if(inputNome.getText().toString().isEmpty()){
+
+        String nome = inputNome.getEditText().getText().toString();
+        String data = inputDataNasc.getEditText().getText().toString();
+        String email = inputEmail.getEditText().getText().toString();
+        String password = inputPassword.getEditText().getText().toString();
+        String cpf = inputCpf.getEditText().getText().toString();
+        String telefone = inputTelefone.getEditText().getText().toString();
+
+        if(uc.validarNome(nome) != ""){
+            inputNome.getEditText().setError(uc.validarNome(nome));
+            return false;
+        }
+
+        if(uc.validarDataNasc(data) != ""){
+            inputDataNasc.getEditText().setError(uc.validarDataNasc(data));
+            return false;
+        }
+
+        if(uc.validarEmail(email) != ""){
+            inputEmail.getEditText().setError(uc.validarEmail(email));
+            return false;
+        }
+
+        if(uc.validaPassword(password) != ""){
+            inputPassword.getEditText().setError(uc.validaPassword(password));
+            return false;
+        }
+
+        if(uc.validarTelefone(telefone) != ""){
+            inputTelefone.getEditText().setError(uc.validaPassword(password));
+        }
+
+        if(uc.validaCpf(ValidaCPF.cleanCPF(cpf)) != ""){
+            inputCpf.getEditText().setError(uc.validaCpf(ValidaCPF.cleanCPF(cpf)));
+            return false;
+        }
+
+        /*if(inputNome.getText().toString().isEmpty()){
             //textInputNome.setErrorTextAppearance();
             inputNome.setError(getString(R.string.error_nome_completo));
             return false;
         }else{
             textInputNome.setErrorEnabled(false);
-        }
+        }*/
+
         return true;
+
     }
 
 }
