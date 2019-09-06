103c6fe13ea375fe2cda2f85fa179ce4a3b31ca7
pedronardelli <pedronardelli2104@gmail.com>
2019-09-06 00:08:22 -0300

    Tela Meus Dados Instituição.

--- app/src/main/java/br/com/acolher/view/AlterarSenha.java
+++ app/src/main/java/br/com/acolher/view/AlterarSenha.java
@@ -13,5 +13,7 @@ public class AlterarSenha extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alterar_senha);
         getSupportActionBar().hide();
+
     }
+
 }
