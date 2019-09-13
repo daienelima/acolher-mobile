6e657102e1d41b70f021474fdf9d7f700dc37e50
HeloisaGalvao <raygalvao96>
2019-09-13 11:21:03 -0300

    Alteração na classe ServiceApi

--- app/src/main/java/br/com/acolher/service/ServiceApi.java
+++ app/src/main/java/br/com/acolher/service/ServiceApi.java
@@ -1,4 +1,12 @@
 package br.com.acolher.service;
 
+import br.com.acolher.model.Instituicao;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
 public interface ServiceApi {
+
+    @POST("/instituicao")
+    Call<Instituicao> cadastroInstituicao(@Body Instituicao instituicao);
 }
