d495bcf0cbcb8324494622ed1dbf37e40d735ec5
Mario Ferreira <mariosilvaosbourne@gmail.com>
2019-09-21 19:38:39 -0300

    Inserindo objeto de consultas

---
+++ app/src/main/java/br/com/acolher/model/Status.java
@@ -0,0 +1,7 @@
+package br.com.acolher.model;
+
+public enum Status {
+    DISPONIVEL,
+    CANCELADA,
+    CONFIRMADA;
+}
--- app/src/main/java/br/com/acolher/view/HomeMapFragment.java
+++ app/src/main/java/br/com/acolher/view/HomeMapFragment.java
@@ -1,16 +1,29 @@
 package br.com.acolher.view;
 
+import android.Manifest;
+import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
@@ -19,16 +32,29 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 import br.com.acolher.R;
+import br.com.acolher.model.Consulta;
+import br.com.acolher.model.Status;
+import br.com.acolher.model.Usuario;
 
-public class HomeMapFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
+public class HomeMapFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener ,GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     GoogleMap mMap;
     MapView mMapView;
     View mView;
     Marker myMarker;
     TextView btnClose;
+    GoogleApiClient googleApiClient;
+    Double latitude;
+    Double longitude;
+    FusedLocationProviderClient fusedLocation;
 
     @Nullable
     @Override
@@ -42,12 +68,36 @@ public class HomeMapFragment extends Fragment implements OnMapReadyCallback, Goo
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        fusedLocation = LocationServices.getFusedLocationProviderClient(getContext());
+
         mMapView = (MapView) mView.findViewById(R.id.map);
         if(mMapView != null){
             mMapView.onCreate(null);
             mMapView.onResume();
             mMapView.getMapAsync(this);
         }
+
+        if(googleApiClient == null){
+
+            googleApiClient = new GoogleApiClient.Builder(getContext())
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+
+        }
+
+        List<Consulta> consultas = new ArrayList<Consulta>();
+
+
+        Consulta c1 = new Consulta();
+        c1.setData(new Date());
+        c1.setHora("10:00");
+        c1.setStatus(Status.DISPONIVEL);
+        c1.setCodigoPaciente(1000);
+        c1.setCodigoProfissional(1000);
+        c1.setCodigoEndereco(1000);
+
     }
 
     /**
@@ -61,13 +111,35 @@ public class HomeMapFragment extends Fragment implements OnMapReadyCallback, Goo
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        LatLng sydney = new LatLng(-34, 151);
+
         mMap = googleMap;
-        myMarker = mMap.addMarker(new MarkerOptions()
-                .position(sydney)
-                .title("Marker in Sydney")
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.person_pin)));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        if (GetLocalization(getContext())) {
+            if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                    ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                return;
+            } else {
+                fusedLocation.getLastLocation().addOnSuccessListener(getActivity(), new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if (location != null) {
+                            latitude = location.getLatitude();
+                            longitude = location.getLongitude();
+
+                            LatLng myLocal = new LatLng(latitude, longitude);
+                            myMarker = mMap.addMarker(new MarkerOptions()
+                                    .position(myLocal)
+                                    .title("Marker in Sydney")
+                                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.person_pin)));
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myLocal, 13.0f));
+
+                            Toast.makeText(getContext(), "LAT E LONG" + latitude + " - " + longitude, Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        }
 
         googleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
             @Override
@@ -99,7 +171,52 @@ public class HomeMapFragment extends Fragment implements OnMapReadyCallback, Goo
     }
 
     @Override
+    public void onResume() {
+        super.onResume();
+        googleApiClient.connect();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (googleApiClient.isConnected()) {
+            googleApiClient.disconnect();
+        }
+    }
+
+    @Override
     public boolean onMarkerClick(Marker marker) {
         return false;
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.i("location", "Connected");
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i("location", "ConnectionSuspended");
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.i("location", "ConnectionFailed");
+    }
+
+    public boolean GetLocalization(Context context){
+        int REQUEST_PERMISSION_LOCALIZATION = 221;
+        boolean res=true;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                res = false;
+                ActivityCompat.requestPermissions((Activity) context, new String[]{
+                                Manifest.permission.ACCESS_FINE_LOCATION},
+                        REQUEST_PERMISSION_LOCALIZATION);
+
+            }
+        }
+        return res;
     }
 }
