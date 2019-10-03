bd8ccf06196d050af273e57f55bee55f404b068e
daienelima <daiene.m17@gmail.com>
2019-10-02 23:07:29 -0300

    ajuste viaCEP

--- app/src/main/java/br/com/acolher/service/ServiceApi.java
+++ app/src/main/java/br/com/acolher/service/ServiceApi.java
@@ -6,10 +6,12 @@ import br.com.acolher.model.Consulta;
 import br.com.acolher.model.Endereco;
 import br.com.acolher.model.Instituicao;
 import br.com.acolher.model.Usuario;
+import br.com.acolher.model.ViaCep;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface ServiceApi {
 
@@ -27,5 +29,8 @@ public interface ServiceApi {
   
     @POST("consulta")
     Call<Consulta> cadastroConsulta (@Body Consulta consulta);
+
+    @GET("{cep}/json")
+    Call<ViaCep> buscarCEP(@Path("cep") String cep);
 
 }
