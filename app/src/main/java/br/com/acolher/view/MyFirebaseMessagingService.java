681bf4e574dcdb543e5e96e8eda42a2ca3675787
Mario Ferreira <mariosilvaosbourne@gmail.com>
2019-11-05 00:33:06 -0300

    Andamento

---
+++ app/src/main/java/br/com/acolher/view/MyFirebaseMessagingService.java
@@ -0,0 +1,7 @@
+package br.com.acolher.view;
+
+import com.google.firebase.iid.FirebaseInstanceIdReceiver;
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+}
